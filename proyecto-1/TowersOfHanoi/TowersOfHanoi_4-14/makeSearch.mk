ec514ffa3ec49d8c2549a9f21e54f5cb5b70aa87
yariluciani <yari.luciani95@gmail.com>
2018-02-20 22:52:47 -0400

    Agrega PDBs de Towers of Hanoi y Rubiks Cube

---
+++ proyecto-1/TowersOfHanoi/TowersOfHanoi_4-14/makeSearch.mk
@@ -0,0 +1,63 @@
+# to make a PDB starting from foo.psvn  type the command:     make pdb ss=foo
+# if $(absname).txt exists it will be used as the input (abstraction commands) to build the abstraction,
+#   otherwise the user will enter the abstraction commands interactively.
+
+CC = gcc
+CXX = g++
+OPT = -Wall -O3 -Wno-unused-function -Wno-unused-variable
+PSVNOPT = --state_map --backward_moves --fwd_history_len=3 --bwd_history_len=0
+
+psvn2c_core.c:
+	cp ../../../psvn/src/psvn2c_core.c ./psvn2c_core.c
+
+psvn2c_state_map.c:
+	cp ../../../psvn/src/psvn2c_state_map.c ./psvn2c_state_map.c
+
+psvn2c_abstraction.c:
+	cp ../../../psvn/src/psvn2c_abstraction.c ./psvn2c_abstraction.c
+
+%.c: %.psvn psvn2c_core.c psvn2c_state_map.c psvn2c_abstraction.c
+	../../../psvn/bin/psvn2c $(PSVNOPT) --name=$(*F) < $< > $@
+	rm -f ./psvn2c_core.c ./psvn2c_state_map.c ./psvn2c_abstraction.c
+
+.PRECIOUS: %.c
+
+priority_queue.hpp:
+	cp ../../Global/priority_queue.hpp ./priority_queue.hpp
+
+node.hpp:
+	cp ../../Global/node.hpp ./node.hpp
+
+
+%.succ: %.c ../../../psvn/global/succ.c
+	$(CC) $(OPT) ../../../psvn/global/succ.c -include $< -o $@
+	rm -f $*.c
+
+%.dist: %.c ../../../psvn/global/dist.cpp
+	$(CXX) $(OPT) ../../../psvn/global/dist.cpp -include $< -o $@
+	rm -f $*.c
+
+%.distSummary: %.c ../../../psvn/global/distSummary.cpp
+	$(CXX) $(OPT) ../../../psvn/global/distSummary.cpp -include $< -o $@
+	rm -f $*.c
+
+abstractor:
+	$(CXX) $(OPT) ../../../psvn/src/abstractor.cpp ../../../psvn/src/psvn.cpp -o $@
+
+%.pdb: abstractor
+	@rm -f `dirname $*`_`basename $*`.{abst,pdb,psvn}
+	./abstractor `dirname $*`.psvn `dirname $*`_`basename $*` < `basename $*`.txt
+	make `dirname $*`_`basename $*`.distSummary
+	echo Calculating `dirname $*`_`basename $*.pdb` ...
+	@./`dirname $*`_`basename $*`.distSummary `dirname $*`_`basename $*`.pdb
+
+%.dist_pdb: %.c ../../../psvn/global/dist_pdb.cpp
+	$(CXX) $(OPT) ../../../psvn/global/dist_pdb.cpp -include $< -o $@
+	rm -f $*.c
+
+%.idaStar: %.c ../../Global/idaStar.cpp priority_queue.hpp node.hpp
+	$(CXX) $(OPT) ../../Global/idaStar.cpp -include $< -include towers-of-hanoi-4-14_PDB.cpp -o $@
+
+.PHONY: clean
+clean:
+	rm -fr *.succ *.dist *.distSummary *.dist_pdb psvn2c_core.c psvn2c_state_map.c psvn2c_abstraction.c abstractor *.dSYM *.o *~
---
+++ proyecto-1/TowersOfHanoi/TowersOfHanoi_4-18/towers-of-hanoi-4-18_PDB.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+state_map_t *map1;
+state_map_t *map2;
+abstraction_t *abs1;
+abstraction_t *abs2;
+state_t abs_state1;
+state_t abs_state2;
+unsigned maximum;
+
+void loadPDB(){
+	FILE *f1 = fopen("towers-of-hanoi-4-18_abst_4-18_1.pdb","r");
+	FILE *f2 = fopen("towers-of-hanoi-4-18_abst_4-18_2.pdb","r");
+
+	abs1 = read_abstraction_from_file("towers-of-hanoi-4-18_abst_4-18_1.abst");
+	map1 = read_state_map(f1);
+
+	abs2 = read_abstraction_from_file("towers-of-hanoi-4-18_abst_4-18_2.abst");
+	map2 = read_state_map(f2);
+
+	fclose(f1);
+	fclose(f2);
+}
+
+unsigned heuristic(state_t puzzle_state){
+	abstract_state(abs1, &puzzle_state, &abs_state1);
+	abstract_state(abs2, &puzzle_state, &abs_state2);
+
+	maximum = std::max(*state_map_get(map1, &abs_state1), *state_map_get(map2, &abs_state2));
+
+	return (maximum);
+}
